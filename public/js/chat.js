9f1a2423684fccc740f1343ddd9a1bad793bd4a5
wm <monterowilken311@gmail.com>
2020-02-23 14:48:48 +0800

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,14 +16,34 @@ function scrollToBottom () {
   }
 }
 
-
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
+
+
+socket.on('updateUserList', function (users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
+
 
 socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
@@ -36,7 +56,6 @@ socket.on('newMessage', function (message) {
 
   jQuery('#messages').append(html);
   scrollToBottom();
-
 });
 
 socket.on('newLocationMessage', function (message) {
